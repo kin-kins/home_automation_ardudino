75b9974ef2aa6e27f28093ac584e496661032348
Ashu Kumar <30663362+kin-kins@users.noreply.github.com>
2018-05-31 02:03:03 +0530

    Add files via upload

---
+++ temp_dsb1280.ino
@@ -0,0 +1,18 @@
+#include<OneWire.h>
+#include<DallasTemperature.h>
+#define wire 2
+
+OneWire oneWire (wire);
+DallasTemperature sensors (&oneWire);
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+sensors.begin();
+}
+
+void loop() {
+  sensors.requestTemperatures();
+  Serial.println(sensors.getTempCByIndex(0));
+  delay(1000);
+ 
+}
