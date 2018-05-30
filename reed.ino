a198f05032ff00a78de5afb2fba38395aed3318b
Ashu Kumar <30663362+kin-kins@users.noreply.github.com>
2018-05-31 02:03:37 +0530

    Add files via upload

---
+++ reed.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(3,OUTPUT);
+pinMode(4,INPUT);
+Serial.begin(9600);
+}
+int z;
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(3,HIGH);
+  
+z=digitalRead(4);
+Serial.println(z);
+delay(1000);
+}
